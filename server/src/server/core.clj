3da4bc271359d27b479e754536d4216bb2894fa0
Alan MacDougall <smoke@alanmacdougall.com>
2013-08-20 22:00:48 -0400

    Implement users resource retrieval, plus create-user stub.
    GET "users" and GET "user/:id" now return valid user JSON, with
    timestamps coerced to their string representation. POST "user/new"
    invokes the stub create-user resource.

--- server/src/server/core.clj
+++ server/src/server/core.clj
@@ -8,28 +8,53 @@
 
 (def db-spec "postgres://postgres:postgres@localhost:5432/raiseyourgame-dev")
 
-;; TODO: replace with a timestamps-to-strings function instead
-(defn resultset-to-users [resultset]
-  (map #(assoc % :created (-> % :created .toString)) resultset))
+(defn created-to-string [m]
+  (assoc m :created (-> m :created .toString)))
 
 (defresource users []
   :available-media-types ["application/json"]
-  :handle-ok
+  :exists?
   (fn [_]
-    (-> (jdbc/query db-spec (sql/select * :users))
-      resultset-to-users
-      json/write-str)))
+    {:users (jdbc/query db-spec (sql/select * :users))})
+  :handle-ok
+  (fn [context]
+    (json/write-str (map created-to-string (:users context)))))
 
 (defresource user [id]
+  :allowed-methods [:get]
   :available-media-types ["application/json"]
-  :handle-ok
+  :exists?
   (fn [_]
-    ;; TODO: actual implementation
-    nil))
+    (if-let [resultset (jdbc/query db-spec
+                         (sql/select * :users
+                           (sql/where {:id (. Integer parseInt id)})))]
+      {:user (first resultset)}))
+  :handle-ok
+  (fn [context]
+    (json/write-str (created-to-string (:user context))))
+  :put!
+  (fn [context]
+    (let [body (slurp (get-in context [:request :body]))
+          data (json/read-str body)]
+      ;; TODO: update in database
+      nil)))
+
+(defresource create-user []
+  :allowed-methods [:get :post]
+  :available-media-types ["text/plain"]
+  ;; TODO: authenticate
+  :handle-ok
+  (fn [_] "Please post to this URL with a JSON body.")
+  :post!
+  (fn [context]
+    (let [body (slurp (get-in context [:request :body]))
+          data (json/read-str body)]
+      ;; TODO: insert into database
+      nil)))
 
 (defroutes app
   (GET "/users" [] (users))
-  (GET "/user/:id" [id] (user id)))
+  (GET "/user/:id" [id] (user id))
+  (POST "/user" [] (create-user)))
 
-;; okay to join? false?
 (run-jetty #'app {:join? false :port 3000})
