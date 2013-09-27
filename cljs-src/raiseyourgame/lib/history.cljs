cd23d16b05cbe60ca97a476bcd1cea270cc7325b
Alan MacDougall <smoke@alanmacdougall.com>
2013-09-26 22:31:25 -0400

    Add history.js and CLJS wrapper.

---
+++ cljs-src/raiseyourgame/lib/history.cljs
@@ -0,0 +1,14 @@
+(ns raiseyourgame.lib.history)
+
+;; Takes function expecting a history state
+(defn bind-navigation-handler [f]
+  (let [history (.-History js/window)
+        adapter (.-Adapter history)
+        state-change-handler (fn [] (f (.getState history)))]
+    (.bind adapter js/window "statechange" state-change-handler)))
+
+(defn push-state [pathname]
+  (.pushState (.-History js/window) nil nil pathname))
+
+(defn state->route [state]
+  (.-hash state))
