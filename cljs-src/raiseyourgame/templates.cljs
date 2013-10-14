efa21085eeab159d6072462fd0bcfcd13214c690
Alan MacDougall <smoke@alanmacdougall.com>
2013-10-14 14:05:31 -0400

    Final templating step: use templates and snippets.

--- cljs-src/raiseyourgame/templates.cljs
+++ cljs-src/raiseyourgame/templates.cljs
@@ -4,11 +4,19 @@
             [enfocus.effects :as effects])
   (:require-macros [enfocus.macros :as em]))
 
+(defn- template [filename]
+  (str "/static/templates/" filename))
+
+(em/defsnippet video-item (template "video_list.html") "li.video-item" [video]
+  "span.title" (ef/content (.-title video))
+  "span.description" (ef/content (.-description video)))
+
+(em/deftemplate home-template (template "video_list.html") [videos]
+  "ul.video-list" (ef/content (map #(video-item %) videos)))
+
 (defn home [context]
-  (at ["#video-list li.video-item"]
+  (at ["#template"]
     ; video list is a JS array of JS objects; see issue #1
-    (em/clone-for [video (:videos context)]
-      "span.title" (ef/content (.-title video))
-      "span.description" (ef/content (.-description video)))))
+    (ef/content (home-template (:videos context)))))
 
 (defn video [context])
