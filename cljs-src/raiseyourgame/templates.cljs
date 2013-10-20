a6063b4387d5443fc3fdf1c1760c362c008efd9c
Alan MacDougall <smoke@alanmacdougall.com>
2013-10-20 12:02:20 -0400

    Create base video view.
    Right now, this view just throws up a placeholder, a title, and a
    description; it's a pared-down version of the Jednotka single-post blog
    view. But the next step is to embed some Youtube in it.

--- cljs-src/raiseyourgame/templates.cljs
+++ cljs-src/raiseyourgame/templates.cljs
@@ -24,11 +24,11 @@
     ; video list is a JS array of JS objects; see issue #1
     (ef/content (home-view (:videos context)))))
 
-(em/defsnippet video-view (template "video_list.html") ".video-item" [video]
-  ".image-link img" (ef/set-attr :src (.-thumbnail video))
-  ".detail-link" (ef/set-attr :href (video-link video))
-  ".title" (ef/content (.-title video))
-  ".description" (ef/content (.-description video)))
+; TODO: this will have to be considerably more complex to include
+; annotations, if nothing else
+(em/defsnippet video-view (template "video.html") ".video" [video]
+  ".detail .title" (ef/content (.-title video))
+  ".detail .description p" (ef/content (.-description video)))
 
 (defn video [context]
   (at ["#main-content .container"]
