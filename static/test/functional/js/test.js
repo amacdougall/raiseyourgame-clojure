33daa79ecd46853e03a16e5a1170c5fe7d7623fd
Alan MacDougall <smoke@alanmacdougall.com>
2013-12-15 13:29:35 -0500

    Test home page rendering; improve teardown; test helpers.

--- static/test/functional/js/test.js
+++ static/test/functional/js/test.js
@@ -1,19 +1,40 @@
-module("raiseyourgame", {
+var moduleDefaults = {
   setup: function() {
     F.open("http://local.raiseyourga.me");
   },
 
+  teardown: function() {}
+};
+
+var videoModuleDefaults = {
+  setup: moduleDefaults.setup,
   teardown: function() {
-    // TODO: base on pause readiness instead?
-    console.log("waiting 5 seconds...");
-    F.wait(5000, function() {
-      console.log("pausing...");
-      F.eval("raiseyourgame.lib.youtube.pause();");
-    });
+    F.eval("raiseyourgame.lib.youtube.force_pause();");
   }
+}
+
+function section() {
+  var name = arguments[0];
+  var options = arguments[1];
+  module(name, options || moduleDefaults);
+}
+
+
+section("home page");
+
+test("video thumbs render", function() {
+  F(".detail-link:first img")
+    .attr("src", "http://i1.ytimg.com/vi/7M2_4sI6I8Y/mqdefault.jpg",
+      "first video thumb had correct thumbnail image");
+  F(".detail-link:first .description")
+    .text("Daigo vs. Gamerbee during Evo 2012.",
+      "first video thumb description was correct");
 });
 
-test("click a video", function() {
+
+section("video page", videoModuleDefaults);
+
+test("click a home page video", function() {
   F("a.detail-link:first").click();
-  F("div.video").visible("video container should be shown");
+  F("div.video").visible("video container was shown");
 });
