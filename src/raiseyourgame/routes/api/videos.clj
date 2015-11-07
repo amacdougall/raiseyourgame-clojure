1dd2c1958314ebfff48f1e9f812c2bec01b66d27
Alan MacDougall <smoke@alanmacdougall.com>
2015-11-07 15:14:34 -0500

    Split API routes into one file per high-level context.

---
+++ src/raiseyourgame/routes/api/videos.clj
@@ -0,0 +1,32 @@
+(ns raiseyourgame.routes.api.videos
+  (:require [raiseyourgame.db.core :as db]
+            [raiseyourgame.schemata :refer :all]
+            [raiseyourgame.models.user :as user]
+            [raiseyourgame.models.video :as video]
+            [ring.util.http-response :refer :all]
+            [compojure.api.sweet :refer :all]
+            [taoensso.timbre :refer [debug]]))
+
+(def api-context
+  (context* "/videos" []
+    ; lookup by id
+    (GET* "/:video-id" []
+          :return Video
+          :path-params [video-id :- Long]
+          :summary "Numeric video id."
+          (if-let [video (video/lookup {:video-id video-id})]
+            (ok video)
+            (not-found "No video matched your request.")))
+
+    ; create
+    (POST* "/" request
+           :body-params [user-id :- Long
+                         url :- String
+                         title :- String
+                         blurb :- String
+                         description :- String]
+           ;; Return private representation of the video, with Location header.
+           (let [video (video/create! (:body-params request))
+                 location (format "/api/videos/%d" (:video-id video))
+                 response (created video)]
+             (assoc-in response [:headers "Location"] location)))))
