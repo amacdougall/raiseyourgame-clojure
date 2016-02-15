4f6971e8ebfe250d8915c2d542e5138621a218e4
Alan MacDougall <smoke@alanmacdougall.com>
2016-02-15 10:50:43 -0500

    Organize and update dependencies.

--- project.clj
+++ project.clj
@@ -3,47 +3,65 @@
   :url "https://raiseyourgame.com"
 
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.nrepl "0.2.10"]
+
+                 ; templating
                  [selmer "0.9.0"]
+                 [markdown-clj "0.9.69"]
+
+                 ; logging
                  [com.taoensso/timbre "4.1.1"]
                  [ring-logger-timbre "0.7.0"]
                  [com.taoensso/tower "3.0.2"]
-                 [markdown-clj "0.9.69"]
+
+                 ; load environment variables and profile.clj props
                  [environ "1.0.0"]
-                 [compojure "1.4.0"]
-                 [clj-time "0.11.0"]
+
+                 ; data wrangling
                  [camel-snake-kebab "0.3.2"]
+                 [clj-time "0.11.0"]
+
+                 ; HTTP webapp system
+                 [compojure "1.4.0"]
                  [ring-webjars "0.1.1"]
                  [ring/ring-defaults "0.1.5"]
                  [ring-ttl-session "0.1.1"]
                  [ring "1.4.0"
                   :exclusions [ring/ring-jetty-adapter]]
+                 [org.immutant/web "2.0.2"] ; instead of jetty
                  [metosin/ring-middleware-format "0.6.0"]
                  [metosin/ring-http-response "0.6.3"]
                  [clout "2.1.2"]
-                 [bouncer "0.3.3"]
+
+                 ; fancy API routes
+                 [metosin/compojure-api "0.24.3"]
+                 [metosin/ring-swagger-ui "2.1.1"]
+
+                 ; validation
+                 [bouncer "0.3.3"] ; manual validation
+                 [prismatic/schema "1.0.4"] ; implicit validation
+                 [org.clojure/test.check "0.9.0"] ; required for Prismatic Schema random generators
+
+                 ; exception reporting middleware
                  [prone "0.8.2"]
-                 [org.clojure/tools.nrepl "0.2.10"]
-                 [org.webjars/bootstrap "3.3.5"]
-                 [org.webjars/jquery "2.1.4"]
+
+                 ; authentication
                  [buddy "0.6.1"]
+
+                 ; database
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]
                  [migratus "0.8.4"]
                  [conman "0.1.7"]
                  [squirrel "0.1.1"]
-                 [org.postgresql/postgresql "9.4-1201-jdbc41"]
+
+                 ;; CLOJURESCRIPT
+                 ; core
                  [org.clojure/clojurescript "1.7.107" :scope "provided"]
                  [org.clojure/tools.reader "0.9.2"]
-                 [reagent "0.5.1-rc"]
-                 [cljsjs/react "0.13.3-1"]
-                 [reagent-forms "0.5.6"]
-                 [reagent-utils "0.1.5"]
-                 [secretary "1.2.3"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [cljs-ajax "0.3.14"]
-                 [prismatic/schema "1.0.4"]
-                 [org.clojure/test.check "0.9.0"] ; required for Prismatic Schema random generators
-                 [metosin/compojure-api "0.24.3"]
-                 [metosin/ring-swagger-ui "2.1.1"]
-                 [org.immutant/web "2.0.2"]]
+                 [reagent "0.6.0-alpha"]
+                 [re-frame "0.7.0-alpha-2"]
+                 [org.clojure/core.async "0.2.374"]
+                 [cljs-ajax "0.5.3"]]
 
   :min-lein-version "2.0.0"
   :uberjar-name "raiseyourgame.jar"
@@ -95,9 +113,9 @@
                                  [peridot "0.4.1"]
                                  [pjstadig/humane-test-output "0.7.0"]
                                  [org.clojure/tools.nrepl "0.2.10"]
-                                 [lein-figwheel "0.3.7"]
+                                 [lein-figwheel "0.5.0-6"]
                                  [mvxcvi/puget "0.8.1"]]
-                  :plugins [[lein-figwheel "0.3.7"]
+                  :plugins [[lein-figwheel "0.5.0-6"]
                             [com.jakemccrary/lein-test-refresh "0.10.0"]]
                   :test-refresh {:notify-command ["lein-test-refresh-notify"]
                                  :notify-on-success true
