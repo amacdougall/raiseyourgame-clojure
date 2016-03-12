3c217de1a901f840571f6feb0dceab50ffdfa0f0
Alan MacDougall <smoke@alanmacdougall.com>
2016-03-12 12:33:24 -0500

    Dispatch client-side route on app load.

--- src-cljs/admin/raiseyourgame/core.cljs
+++ src-cljs/admin/raiseyourgame/core.cljs
@@ -5,7 +5,11 @@
             [raiseyourgame.routes]
             [reagent.core :as reagent]
             [re-frame.core :refer [dispatch dispatch-sync]]
-            [devtools.core :as devtools]))
+            [secretary.core :as secretary]
+            [goog.events :as events]
+            [goog.history.EventType :as EventType]
+            [devtools.core :as devtools])
+  (:import goog.History))
 
 (devtools/enable-feature! :sanity-hints :dirac)
 (devtools/install!)
@@ -13,6 +17,18 @@
 (defn main []
   (dispatch-sync [:initialize-db])
   (reagent/render [views/main-view]
-                  (.getElementById js/document "app")))
+                  (.getElementById js/document "app"))
+
+  ; run initial client-side route, if any
+  (secretary/dispatch! (.-pathname (.-location js/window)))
+
+  ; handle future navigation events
+  (doto (History.)
+    (events/listen
+      EventType/NAVIGATE
+      (fn [event]
+        (.log js/console "navigation event %o" event)
+        ))
+    (.setEnabled true)))
 
 (main)
