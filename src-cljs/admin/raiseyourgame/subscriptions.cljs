2033fa97b636ae0b628899718c0a34fb263c11b3
Alan MacDougall <smoke@alanmacdougall.com>
2016-04-10 15:46:55 -0400

    Refactor re-frame subscriptions to use pure functions.

--- src-cljs/admin/raiseyourgame/subscriptions.cljs
+++ src-cljs/admin/raiseyourgame/subscriptions.cljs
@@ -2,27 +2,31 @@
   (:require [re-frame.core :refer [register-sub]])
   (:require-macros [reagent.ratom :refer [reaction]]))
 
-(register-sub
-  :current-user-query
-  (fn [db _]
-    (reaction (:current-user @db))))
+;; Given a query function, returns a function which expects the app db and a
+;; re-frame subscription values vector [sid & args], and returns a reaction.
+;;
+;; This helper function makes it more efficient to register subscriptions in
+;; terms of pure query functions.
+(defn- reactionize [f]
+  (fn [db values]
+    (reaction (f @db values))))
 
-(register-sub
-  :form-values-query
-  (fn [db [_ form-id]]
-    (reaction (get-in @db [:forms form-id :values]))))
+(defn current-user-query [db _]
+  (:current-user db))
+(register-sub :current-user-query (reactionize current-user-query))
 
-(register-sub
-  :form-errors-query
-  (fn [db [_ form-id]]
-    (reaction (get-in @db [:forms form-id :errors]))))
+(defn form-values-query [db [_ form-id]]
+  (get-in db [:forms form-id :values]))
+(register-sub :form-values-query (reactionize form-values-query))
 
-(register-sub
-  :target-type-query
-  (fn [db _]
-    (reaction (get-in @db [:target :type]))))
+(defn form-errors-query [db [_ form-id]]
+  (get-in db [:forms form-id :errors]))
+(register-sub :form-errors-query (reactionize form-errors-query))
 
-(register-sub
-  :users-query
-  (fn [db _]
-    (reaction (get-in @db [:target :users]))))
+(defn target-type-query [db _]
+  (get-in db [:target :type]))
+(register-sub :target-type-query (reactionize target-type-query))
+
+(defn users-query [db _]
+  (get-in db [:target :users]))
+(register-sub :users-query (reactionize users-query))
